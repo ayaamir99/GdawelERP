164aca72e2b9d91bff892dc7b2095f0c40fdb903
ayaamir99 <ayak77431@gmail.com>
2024-03-03 15:29:46 +0200

    3/3

--- src/test/java/org/example/StepDefinition/D02_LoginStepDef.java
+++ src/test/java/org/example/StepDefinition/D02_LoginStepDef.java
@@ -3,7 +3,9 @@ package org.example.StepDefinition;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import org.example.Pages.P02_LoginPage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
@@ -12,15 +14,20 @@ import static org.example.StepDefinition.Hooks.driver;
 
 public class D02_LoginStepDef {
     P02_LoginPage login=new P02_LoginPage();
-    //     Given I am on the login page
-    @And("user enter valid email{string} and valid password{string}")
-    public void validEmailAndPassword(){
+    @And("I click on Account then SignIn")
+    public void NavigateToLoginScreen(){
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.navigate().to("https://gdawel.app/login");
-        ///,mcm,msml,sm
 
     }
-    @And("user enter valid email{string} and valid password{string}")
+    @And("Given I am on the login page")
+    public void user_is_on_login_page(){
+        String expected = "https://gdawel.app/login";
+        String LoginUrl = driver.getCurrentUrl();
+        Assert.assertEquals(LoginUrl, expected);
+
+    }
+    @And("I enter valid email{string} and valid password{string}")
     public void validEmailAndPassword(String email,String password){
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         try {
@@ -30,6 +37,17 @@ public class D02_LoginStepDef {
         }
         login.sendKeys(login.EmailLoginButton(),"detebag627@elixirsd.com");
         login.sendKeys(login.passwordLoginButton(),""+74108520);
+
+    }
+    @And(" And I user click on SignIn Button")
+    public void clickOnSignInButton(String email,String password){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         login.click(login.loginButton());
 
     }
--- src/test/java/org/example/StepDefinition/D03_AddProductStepDef.java
+++ src/test/java/org/example/StepDefinition/D03_AddProductStepDef.java
@@ -1,4 +1,43 @@
 package org.example.StepDefinition;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import org.example.Pages.P02_LoginPage;
+import org.example.Pages.P03_ProductsPage;
+
+import java.time.Duration;
+
+import static org.example.StepDefinition.Hooks.driver;
+
 public class D03_AddProductStepDef {
+
+    P02_LoginPage login=new P02_LoginPage();
+    P03_ProductsPage Products = new P03_ProductsPage();
+    @Given("I Open Products Page")
+    public void NavigateToProductsScreen(){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        //     private final By productsAndWarehouseMenu = By.id("products-menu");
+        //    //    private final By products = By.xpath("//a[@href='https://gdawel.app/dashboard/products']");
+        //    private final By products = By.id("product-list-menu");
+            Products.ClickOnproductsAndWarehouseMenu().click();
+            Products.ClickOnProductsMenu().click();
+    }
+    @And("I enter valid email{string} and valid password{string}")
+    public void validEmailAndPassword(String email,String password){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        login.sendKeys(login.EmailLoginButton(),"detebag627@elixirsd.com");
+        login.sendKeys(login.passwordLoginButton(),""+74108520);
+
+    }
+    @And("I click on Account then SignIn")
+    public void NavigateToLoginScreen(){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.navigate().to("https://gdawel.app/login");
+
+    }
 }
